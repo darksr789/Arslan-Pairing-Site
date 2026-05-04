85ce45f78fd0013472c6369d37ac054d20f18fef
darksr789 <dyt27380@gmail.com>
2026-05-05 00:04:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -76,7 +76,7 @@ router.get('/', async (req, res) => {
 ║❍ 𝐑𝐞𝐩𝐨: https://github.com/darksurya345/SURYA-X 
 ║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/?mode=wwt
 ║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029Vb64JNKJf05UHKREBM1h
-║
+║❍ 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: https://t.me/DARKSURYA_345 
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
 𒂀 Enjoy SURYA-X
