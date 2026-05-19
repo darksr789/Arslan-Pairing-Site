193a5d94ed7193311f1e538abcc4d30caa9bbe6a
darksr789 <dyt27380@gmail.com>
2026-05-20 04:27:16 +0530

    Update arslan.js

--- arslan.js
+++ arslan.js
@@ -6,21 +6,27 @@ const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
+
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
 })
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
+
+// ✅ QR page route — stylish qr.html
+app.use('/qr-page', async (req, res, next) => {
+    res.sendFile(__path + '/qr.html')
 })
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star SURYA-X ⭐ 
-
- Server running on http://localhost:` + PORT)
+    console.log(`\nDon't Forget To Give Star SURYA-X ⭐ \n\n Server running on http://localhost:` + PORT)
 })
 
 module.exports = app
